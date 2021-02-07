6fffea6884ce25a4cc658069efea9dab457b5c94
Md.Rabiul islam <rabiulrabiul1924@gmail.com>
2021-02-07 18:16:34 +0600

    ok

---
+++ script.js
@@ -0,0 +1,83 @@
+const search = document.getElementById("search"),
+  submit = document.getElementById("submit"),
+  mealsElement = document.getElementById("meals"),
+  resultHeading = document.getElementById("result-heading"),
+  single_mealElement = document.getElementById("single-meal");
+function searchMeal(element) {
+  element.preventDefault();
+  single_mealElement.innerHTML = "";
+  const term = search.value;
+  console.log(term);
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        resultHeading.innerHTML = `<h2>Your Search Results '${term}':</h2> 
+        <h3>Look down to see result after click in any image </h3>`;
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>Sorry, we didn't find any meal!</p>`;
+        } else {
+          mealsElement.innerHTML = data.meals
+            .map(
+              (meal) => `
+            <div class="meal">
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+            <div class="meal-info" data-mealID="${meal.idMeal}">
+            <h3>${meal.strMeal}</h3></div>
+            </div>`
+            )
+            .join("");
+        }
+      });
+    search.value = "";
+  } else {
+    alert("Please enter a search term");
+  }
+}
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      addMealToDOM(meal);
+    });
+}
+
+function addMealToDOM(meal) {
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]}-${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+  single_mealElement.innerHTML = `
+  <div class="single-meal">
+    <h1>${meal.strMeal}</h1>
+    <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+    <div class="main">
+        <h2>Ingredients</h2>
+        <ul>
+            ${ingredients.map((ing) => `<li>${ing}</li>`).join("")}
+        </ul>
+    </div>
+  </div>`;
+}
+submit.addEventListener("submit", searchMeal);
+mealsElement.addEventListener("click", (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains("meal-info");
+    } else {
+      return false;
+    }
+  });
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute("data-mealid");
+    getMealById(mealID);
+  }
+});
